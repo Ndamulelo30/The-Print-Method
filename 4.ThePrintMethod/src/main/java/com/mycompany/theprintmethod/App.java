8099e51daa7dadaf8a4107688ba5b230d78f8bfb
Ndamulelo30 <141257791+Ndamulelo30@users.noreply.github.com>
2024-02-16 12:52:15 +0200

    Add files via upload

---
+++ 4.ThePrintMethod/src/main/java/com/mycompany/theprintmethod/App.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.theprintmethod;
+
+/**
+ *
+ * @author Sanele
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.print("Hello World! ");
+        System.out.print("I will print on the same line.");
+        
+    }
+}
